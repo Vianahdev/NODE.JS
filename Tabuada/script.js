8d412c3733cc89e161af13bc843c7e25de00f1fe
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-09-04 14:30:48 -0300

    Update NODE.JS
    Criação de tabuada em NODE.JS utilizando modulos.

---
+++ Tabuada/script.js
@@ -0,0 +1,6 @@
+let tab = require("./tabuada");
+
+let args = process.argv.slice(2);
+let l_num = Number(args[0]);
+
+tab(l_num);
---
+++ Tabuada/tabuada.js
@@ -0,0 +1,6 @@
+module.exports = function Tab(num){
+    for(var i = 0; i <= 10; i++){
+        let l_result = num * i;
+        console.log(i + " X " + num + " = " + l_result);
+    }
+}
