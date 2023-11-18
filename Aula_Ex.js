c786a5d2e695388af93dcf937b979da9735c15ba
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-11-18 18:04:27 -0300

    NODE.JS
    Argumentos em NODE.JS

--- Aula_Ex.js
+++ Aula_Ex.js
@@ -1,6 +1,11 @@
+let a = 10;
+let b = 15;
+let c = Soma(a,b)
+
+
 function Soma(num1, num2){
     return num1 + num2;
 }
 
-console.log("O resultado da soma é:", Soma(4,6));
+console.log("O resultado da soma é:", c);
 
