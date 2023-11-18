d11d248cc4767dd23a29e982f523417e194724f2
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-11-18 18:12:33 -0300

    Update Aula_Args.js

--- Aula_Args.js
+++ Aula_Args.js
@@ -1,2 +1,16 @@
-console.log(process.argv);
+let args = process.argv.slice(2);
+
+let a = Number(args[0]);
+let b = Number(args[1]);
+
+let result = Soma(a,b)
+
+function Soma(num1, num2){
+  return num1 + num2
+}
+
+console.log(args);
+console.log(result);
+
+
 
