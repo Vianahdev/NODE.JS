36b8c52c93027bcbc62a053ca04f6eba252f2c84
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-11-27 11:52:57 -0300

    Image
    Redirecionamento de uma imagem

--- Package/index.js
+++ Package/index.js
@@ -1,5 +1,5 @@
-let calcBasic = require('mathematics');
-let area = require('area');
+const calcBasic = require('mathematics');
+const area = require('area');
 
 console.log(calcBasic.mult(4,5));
 console.log(area.areaQuadrado(10,5));
